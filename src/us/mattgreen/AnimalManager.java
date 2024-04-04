441b7757f8ecb31731e618811c1361969f144dd6
Braedon Fredin <b260fredin@gmail.com>
2024-04-04 16:49:20 -0500

    [1.02] Added next steps for adding an animal.

--- src/us/mattgreen/AnimalManager.java
+++ src/us/mattgreen/AnimalManager.java
@@ -3,21 +3,72 @@ package us.mattgreen;
 import java.util.Scanner;
 
 public class AnimalManager {
-
-    // gets user input
     public Scanner sc = new Scanner(System.in);
 
-    public void createAnimal() {
-        String input = getInput("\n\nWhat type of animal would you like to create?\n\n1) Pet\n2) Human");
-    }
-
-    private String getInput(String prompt) {
+    // parent method
+    public void createAnimalWithInput(){
         String input;
 
+        do {
+
+            input = getInput("\n\nWhat type of animal would you like to create?\n\n1) Pet\n2) Human (Teacher)\n\n> ");
+
+            // pet
+            if(input == "1") {
+                 createPet();
+
+            // human
+            } else if(input == "2") {
+
+                createTeacher();
+
+            // invalid, repeat loop
+            } else {
+                System.out.println("Please select a valid option");
+            }
+
+        } while (input != "1" && input != "2");
+    }
+
+    // helper methods
+    private String getInput(String prompt) {
         System.out.print(prompt);
 
-        input = sc.nextLine();
+        // no validation needed, this will be used in other methods
+        // and each method will have different validation.
+        return sc.nextLine();
+    }
 
-        return input;
+    private void createPet() {
+        String input;
+
+        do {
+
+            input = getInput("\n\nWhich type of Pet would you like to create?\n\n1) Dog\n2) Cat\n\n> ");
+
+            // dog
+            if(input == "1") {
+                //TODO: createDog()
+                createDog();
+
+            // cat
+            } else if(input == "2") {
+                //TODO: createCat()
+                createCat();
+
+            // invalid, repeat loop
+            } else {
+                System.out.println("Please select a valid option");
+            }
+
+        } while (input != "1" && input != "2");
+    }
+
+    private void createCat() {
+
+    }
+
+    private void createTeacher() {
+
     }
 }
